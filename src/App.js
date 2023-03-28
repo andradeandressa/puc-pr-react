1561946acab6461a95596bf80439da61013c6530
andradeandressa <andrade-andressa@outlook.com>
2023-03-27 21:50:12 -0300

    commit Login form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Login extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Login",
+      email: "",
+      password: "",
+      message: ""
+    }
+    this.validation = this.validation.bind(this);
+    this.changeCredentials = this.changeCredentials.bind(this);
+  }
+
+  changeCredentials(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  validation() {
+    var validCredentials = "Acessado com sucesso!";
+    var invalidCredentials = "Usuário ou senha incorretos!";
+    if(this.state.email === "eduardo.lino@pucpr.br" && this.state.password === "123456") {
+      this.setState({message:validCredentials});
+    } else {
+      this.setState({message:invalidCredentials});
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <h1 className="title"> {this.state.title} </h1>
+        <form className="loginForm">
+          <input type="email" name="email" placeholder="E-mail" value={this.state.email} onChange={this.changeCredentials} />
+          <input type="password" name="password" placeholder="Senha" value={this.state.password} onChange={this.changeCredentials} />
+        </form>
+        <button className="loginButton" onClick={this.validation}>Acessar</button>
+        <label name="label">{this.state.message}</label>
+      </div>
+    )
+  } 
+
 }
 
-export default App;
+export default Login;
